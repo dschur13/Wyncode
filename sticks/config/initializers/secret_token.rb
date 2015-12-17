3b411e4a5b01a803f3d2e6319d506a066386eca0
DanSchur <dschur13@gmail.com>
2015-12-16 20:06:54 -0500

    restarted project using rails-api gem and mongoid from the beginning to avoid issues

---
+++ sticks/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Sticks::Application.config.secret_token = '671f48bfc53cab6bedd094620c17659db901b26812b58d6cd683aad67541b6b665ef1ce54b270f7e8a5311be866ea74f32dfc1fcd51f21d600c9e414a7c20525'
